f4bb274a22567de48802932a975007e6175fad2a
Yuliya Frolova <jupunkevich@gmail.com>
2020-03-24 23:25:30 +0300

    Updated code

--- src/main/java/ru.netology.stats/StatsService.java
+++ src/main/java/ru.netology.stats/StatsService.java
@@ -13,11 +13,61 @@ public class StatsService {
     public int findAverageSum(int[] monthSales) {
         StatsService service = new StatsService();
         int sum = service.findTotalSum(monthSales);
-        return (sum / 12);
+        return (sum / monthSales.length);
+    }
+
+    public int findMaxSalesMonth(int[] monthSales) {
+        int indexOfMonth = 0;
+        int index = 0;
+        int maxSales = monthSales[0];
+        for (int sale: monthSales) {
+            index += 1;
+            if (maxSales < sale) {
+                maxSales = sale;
+                indexOfMonth = index;
+            }
+        }
+        return indexOfMonth;
+    }
+
+    public int findMinSalesMonth(int[] monthSales) {
+        int indexOfMonth = 0;
+        int index = 0;
+        int minSales = monthSales[0];
+        for (int sale: monthSales) {
+            index += 1;
+            if (minSales > sale) {
+                minSales = sale;
+                indexOfMonth = index;
+            }
+        }
+        return indexOfMonth;
+    }
+
+    public int findUnderAverageMonths(int[] monthSales) {
+        StatsService service = new StatsService();
+        int averageSum = service.findAverageSum(monthSales);
+        int count = 0;
+        for (int sale : monthSales) {
+            if (sale < averageSum) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int findOverAverageMonths(int[] monthSales) {
+        StatsService service = new StatsService();
+        int averageSum = service.findAverageSum(monthSales);
+        int count = 0;
+        for (int sale : monthSales) {
+            if (sale > averageSum) {
+                count++;
+            }
+        }
+        return count;
     }
 }
-
-
 
 
 
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -13,7 +13,6 @@ class StatsServiceTest {
 
         int actual = service.findTotalSum(monthSales);
         assertEquals(expected, actual);
-
     }
 
     @org.junit.jupiter.api.Test
@@ -26,4 +25,47 @@ class StatsServiceTest {
         int actual = service.findAverageSum(monthSales);
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldFindMaxSalesMonth() {
+        StatsService service = new StatsService();
+
+        int[] monthSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 6;
+
+        int actual = service.findMaxSalesMonth(monthSales);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldFindMinSalesMonth() {
+        StatsService service = new StatsService();
+        int[] monthSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+
+        int actual = service.findMinSalesMonth(monthSales);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldFindUnderAverageMonths() {
+        StatsService service = new StatsService();
+        int[] monthSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.findUnderAverageMonths(monthSales);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldFindOverAverageMonths() {
+        StatsService service = new StatsService();
+
+        int[] monthSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.findOverAverageMonths(monthSales);
+        assertEquals(expected, actual);
+    }
 }
+
